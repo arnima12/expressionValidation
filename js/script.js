b266720ff946e0e6981dfa347ec0ad847699483a
arnima12 <tasnimanzum1234@gmail.com>
2024-02-21 16:33:45 +0600

    fixed

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 function showEmailAlert (){
    var userEmail = prompt("Please type your email");
+   if (userEmail === null || userEmail.trim() === "") {
+    alert("Email field is blank. Please enter a valid email.");
+    return; 
+ }
 console.log(userEmail);
 
 const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
@@ -14,6 +18,10 @@ switch (true) {
 }
 function showNumberAlert(){
    var userPhoneNumber = prompt("Please type your phone number with country code");
+   if (userPhoneNumber === null || userPhoneNumber.trim() === "") {
+    alert("Phone Number field is blank. Please enter a valid phone number.");
+    return; 
+ }
 console.log(userPhoneNumber);
 
 const phoneNumberRegex = /^\+(?:[0-9] ?){6,14}[0-9]$/;
@@ -28,6 +36,10 @@ switch (true) {
 }
 function showPostCodeAlert(){
    var userPostCode = prompt("Please type your postcode:");
+   if (userPostCode === null || userPostCode.trim() === "") {
+    alert("Post Code field is blank. Please enter a valid post code.");
+    return; 
+ }
 console.log(userPostCode);
 
 const postCodeRegex = /^\d{4}$/;
